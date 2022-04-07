fa55c82cdc361e1f1f6aead1dcf114b922bde2ee
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-07 22:22:01 +0300

    adding link to the topbar component

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom"
 import "./topbar.css"
 
 export default function Topbar() {
+  const user = true
   return (
     <div className="top">
       <div className="topLeft">
@@ -11,24 +13,44 @@ export default function Topbar() {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
           <li className="topListItem">ABOUT</li>
           <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          {user && <li className="topListItem">LOGOUT</li>}
         </ul>
       </div>
       <div className="topRight">
-        <img
-          className="topImg"
-          src="https://scontent-muc2-1.xx.fbcdn.net/v/t1.18169-9/11825793_10207545460584331_2274065792767588975_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_ohc=F22hI_8vOJYAX9Tulv7&_nc_oc=AQn7zBRQELPyLMw6iM9hcZjdSsJetFuZuUNeqL6rUwpRLwauV1-r4l0DIpyGs7b9U55fR0-6JkbInXmJkCQzQwPs&_nc_ht=scontent-muc2-1.xx&oh=00_AT_HUuruftXphvFh_n8phU74-Zbyrqvn_Svtgqno316cWg&oe=6274DAD3"
-          alt=""
-        />
-        <ul className="topList">
-          <li className="topListItem">LOGIN</li>
-          <li className="topListItem">REGISTER</li>
-        </ul>
-
+        {user ? (
+          <Link className="link" to="/settings">
+            <img
+              className="topImg"
+              src="https://scontent-muc2-1.xx.fbcdn.net/v/t1.18169-9/11825793_10207545460584331_2274065792767588975_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_ohc=F22hI_8vOJYAX-h1M9w&_nc_oc=AQkhuMFNsIi0K6CRhurvCyueSMOC4AH2_wrFXNccD9bxN02p0RUtgQSTsphJbZKUH6UNT4TRROPR1jte4CTG9OFz&_nc_ht=scontent-muc2-1.xx&oh=00_AT_obUGROxkTtmBPiXGA2iApbvEQrEInI6k57qHBrb17GQ&oe=6274DAD3"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
         <i className="topSearchIcon fas fa-search"></i>
       </div>
     </div>
