8813cbc9686c0dec56aa24356113f3edb2d82721
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-07 22:44:04 +0300

    adding login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,25 @@
 import "./login.css"
 
-function Login() {
-  return <div>Login Page</div>
+export default function Login() {
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm">
+        <label>Email</label>
+        <input
+          className="loginInput"
+          type="text"
+          placeholder="Enter your email..."
+        />
+        <label>Password</label>
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="Enter your password..."
+        />
+        <button className="loginButton">Login</button>
+      </form>
+      <button className="loginRegisterButton">Register</button>
+    </div>
+  )
 }
-
-export default Login
