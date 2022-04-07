a790083b66030ffcce0e32ec8561cb2b1cb544ea
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-08 00:56:34 +0300

    restyling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,9 @@ import "./header.css"
 export default function Header() {
   return (
     <div className="header">
-      <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
-        <span className="headerTitleLg">BLOG</span>
-      </div>
       <img
         className="headerImg"
-        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        src="https://images.unsplash.com/photo-1566385101042-1a0aa0c1268c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
         alt=""
       />
     </div>
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -2,56 +2,61 @@ import { Link } from "react-router-dom"
 import "./topbar.css"
 
 export default function Topbar() {
-  const user = true
+  const user = false
   return (
     <div className="top">
-      <div className="topLeft">
-        <i className="topIcon fab fa-facebook-square"></i>
-        <i className="topIcon fab fa-instagram-square"></i>
-        <i className="topIcon fab fa-pinterest-square"></i>
-        <i className="topIcon fab fa-twitter-square"></i>
+      <div className="topbarTop">
+        <span className="topbarTitle">Gardens in Colors</span>
       </div>
-      <div className="topCenter">
-        <ul className="topList">
-          <li className="topListItem">
-            <Link className="link" to="/">
-              HOME
-            </Link>
-          </li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">
-            <Link className="link" to="/write">
-              WRITE
-            </Link>
-          </li>
-          {user && <li className="topListItem">LOGOUT</li>}
-        </ul>
-      </div>
-      <div className="topRight">
-        {user ? (
-          <Link className="link" to="/settings">
-            <img
-              className="topImg"
-              src="https://scontent-muc2-1.xx.fbcdn.net/v/t1.18169-9/11825793_10207545460584331_2274065792767588975_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_ohc=F22hI_8vOJYAX-h1M9w&_nc_oc=AQkhuMFNsIi0K6CRhurvCyueSMOC4AH2_wrFXNccD9bxN02p0RUtgQSTsphJbZKUH6UNT4TRROPR1jte4CTG9OFz&_nc_ht=scontent-muc2-1.xx&oh=00_AT_obUGROxkTtmBPiXGA2iApbvEQrEInI6k57qHBrb17GQ&oe=6274DAD3"
-              alt=""
-            />
-          </Link>
-        ) : (
+      <div className="topbarbottom">
+        <div className="topLeft">
+          <i className="topIcon fab fa-facebook-square"></i>
+          <i className="topIcon fab fa-instagram-square"></i>
+          <i className="topIcon fab fa-pinterest-square"></i>
+          <i className="topIcon fab fa-twitter-square"></i>
+        </div>
+        <div className="topCenter">
           <ul className="topList">
             <li className="topListItem">
-              <Link className="link" to="/login">
-                LOGIN
+              <Link className="link" to="/">
+                HOME
               </Link>
             </li>
+            <li className="topListItem">ABOUT</li>
+            <li className="topListItem">CONTACT</li>
             <li className="topListItem">
-              <Link className="link" to="/register">
-                REGISTER
+              <Link className="link" to="/write">
+                WRITE
               </Link>
             </li>
+            {user && <li className="topListItem">LOGOUT</li>}
           </ul>
-        )}
-        <i className="topSearchIcon fas fa-search"></i>
+        </div>
+        <div className="topRight">
+          <i className="topSearchIcon fas fa-search"></i>
+          {user ? (
+            <Link className="link" to="/settings">
+              <img
+                className="topImg"
+                src="https://scontent-muc2-1.xx.fbcdn.net/v/t1.18169-9/11825793_10207545460584331_2274065792767588975_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_ohc=F22hI_8vOJYAX-h1M9w&_nc_oc=AQkhuMFNsIi0K6CRhurvCyueSMOC4AH2_wrFXNccD9bxN02p0RUtgQSTsphJbZKUH6UNT4TRROPR1jte4CTG9OFz&_nc_ht=scontent-muc2-1.xx&oh=00_AT_obUGROxkTtmBPiXGA2iApbvEQrEInI6k57qHBrb17GQ&oe=6274DAD3"
+                alt=""
+              />
+            </Link>
+          ) : (
+            <ul className="topList">
+              <li className="topListItem">
+                <Link className="link" to="/login">
+                  LOGIN
+                </Link>
+              </li>
+              <li className="topListItem">
+                <Link className="link" to="/register">
+                  REGISTER
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   )
