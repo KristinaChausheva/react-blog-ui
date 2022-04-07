48a05cc4f15c211df5ee8315b81303a99c3379c9
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-07 22:42:42 +0300

    adding posts post and sidebar components to the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,21 @@
 import "./home.css"
+import { useLocation } from "react-router"
+import Header from "../../components/header/Header"
+import Posts from "../../components/posts/Posts"
+import Sidebar from "../../components/sidebar/Sidebar"
 
-function Home() {
-  return <div>Home page</div>
+function Homepage() {
+  const location = useLocation()
+  console.log(location)
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
+  )
 }
 
-export default Home
+export default Homepage
