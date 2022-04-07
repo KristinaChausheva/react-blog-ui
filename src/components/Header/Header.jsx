8dece4834035f369f06edfd0e1e7e5adb7d7d819
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-07 22:31:50 +0300

    header component implemented

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
-import "header.css"
+import "./header.css"
 
-function Header() {
-  return <div>header</div>
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">BLOG</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+    </div>
+  )
 }
-
-export default Header
