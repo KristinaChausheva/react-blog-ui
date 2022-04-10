4d6734c7c663f9d901477e1b3a764261acb514d1
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-10 18:45:51 +0300

    addign register functionality to the firebase

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,31 +1,64 @@
 import "./register.css"
+import { useState } from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../../firebase"
+import { useNavigate } from "react-router-dom"
 
 export default function Register() {
+  const [error, setError] = useState(false)
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        // const user = userCredential.user
+        navigate("/login")
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        setError(true)
+        console.log(error)
+      })
+  }
+
   return (
     <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleRegister}>
         <label>Username</label>
         <input
           className="registerInput"
           type="text"
           placeholder="Enter your username..."
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label>Email</label>
         <input
           className="registerInput"
           type="text"
           placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label>Password</label>
         <input
           className="registerInput"
           type="password"
           placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="registerButton">Register</button>
+        <button className="registerButton" type="submit">
+          Register
+        </button>
       </form>
-      <button className="registerLoginButton">Login</button>
+      {/* <button className="registerLoginButton">Login</button> */}
     </div>
   )
 }
